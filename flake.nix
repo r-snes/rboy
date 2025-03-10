89b769f286b6156f0a713e942d819ed463bfdbc0
Florent Charpentier <florent.charpentier@epitech.eu>
2025-03-10 15:40:09 +0100

    :sparkles: Flakes and warning fixes

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        devShell = mkShell rec {
+          buildInputs = [
+            libxkbcommon
+            libGL
+
+            # WINIT_UNIX_BACKEND=wayland
+            wayland
+
+            # WINIT_UNIX_BACKEND=x11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+
+            rustc
+            cargo
+            rustfmt
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
+}
