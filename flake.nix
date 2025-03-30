f92917d9ecf6f2724215c423dfa8c4ab00695840
Florent Charpentier <florent.charpentier@epitech.eu>
2025-03-31 01:26:20 +0200

    :snowflake: Moved to recent nixpkgs unstable for Rust 2024

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = {
